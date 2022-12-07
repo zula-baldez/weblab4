3ee4237576fa77c9167338a382b9aee392ae5054
Egor <moyakkauntda@gmail.com>
2022-12-07 10:13:27 +0300

    up

--- src/main/java/com/example/lab4/controller/AccessController.java
+++ src/main/java/com/example/lab4/controller/AccessController.java
@@ -2,12 +2,14 @@ package com.example.lab4.controller;
 
 import com.example.lab4.model.UsersDbController;
 import com.example.lab4.model.User;
+import com.example.lab4.security.PasswordChecker;
 import com.example.lab4.util.HashCoder;
 import com.example.lab4.util.LoginDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,32 +19,22 @@ import java.util.Optional;
 
 @Controller
 @ComponentScan
-public class RegistrationAndLoginController {
+@CrossOrigin
+public class AccessController {
     @Autowired
     private UsersDbController usersDbController;
     @Autowired
     private HashCoder hashCoder;
-
-    public ResponseEntity<String> checkLogin(String login, String password) {
-        Optional<User> userOpt = usersDbController.findByLogin(login);
-        if(userOpt.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        if((hashCoder.hash(password).equals(userOpt.get().getPassword()))) {
-            return new ResponseEntity<String>(HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-    }
-
+    @Autowired
+    private PasswordChecker passwordChecker;
 
     @PostMapping("/try_login")
-    @CrossOrigin
     public ResponseEntity<String> tryLogin(@RequestBody LoginDTO loginDTO) {
-        return checkLogin(loginDTO.getLogin(), loginDTO.getPassword());
+        return passwordChecker.generateJWTAccess(loginDTO.getLogin(), loginDTO.getPassword());
+
     }
     @PostMapping("/try_register")
-    @CrossOrigin
-    public ResponseEntity<String> tryRegister(@RequestBody LoginDTO loginDTO) {
+        public ResponseEntity<String> tryRegister(@RequestBody LoginDTO loginDTO) {
 
         Optional<User> userOpt = usersDbController.findByLogin(loginDTO.getLogin());
         if(userOpt.isPresent() || loginDTO.getLogin() == null || loginDTO.getPassword() == null) {
@@ -52,6 +44,6 @@ public class RegistrationAndLoginController {
         user.setLogin(loginDTO.getLogin());
         user.setPassword(hashCoder.hash(loginDTO.getPassword()));
         usersDbController.save(user);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return passwordChecker.generateJWTAccess(loginDTO.getLogin(), loginDTO.getPassword());
     }
 }
